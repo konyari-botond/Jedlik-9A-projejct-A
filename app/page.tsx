5ae97e97fd637e4aa7a18291c78a7e4a8108734f
konyari-botond <konyari.botond.bator@students.jedlik.eu>
2026-05-06 17:58:16 +0200

    first page - href

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,37 @@
-"use client";
+"use client"
 
-import { clsx } from "clsx";
-import dayjs from "dayjs";
-import { SunMoon } from "lucide-react";
-import Image from "next/image";
-import { useEffect } from "react";
-import toast from "react-hot-toast";
-import { useGlobalStore } from "@/store/globalStore";
+import Link from "next/link"
 
-export default function HomePage() {
-  // Using Zustand global store for state management example
-  const { gs, set } = useGlobalStore();
+type LinkType = {
+    href: string;
+    label: string;
+}
 
-  useEffect(() => {
-    toast.success(`Render on: ${dayjs().format("YYYY.MM.DD HH:mm:ss")}`);
-  }); // no dependency array to demonstrate re-render toast
+const links: LinkType[] = [
+    {href: "/szamolas", label: "Két autó mozgása egymással szembe"}
+]
 
-  function handleThemeToggle() {
-    set("lightTheme", !gs.lightTheme);
-    document.documentElement.classList.toggle("dark", gs.lightTheme);
-  }
 
+export default function KezdoOldal() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-200 py-2 dark:bg-gray-800">
-      <h1 className={clsx("mb-6 text-3xl font-bold", gs.lightTheme ? "text-black" : "text-white")}>
-        {"Hello, "}
-        {gs.loggedUser || (
-          <Image
-            alt="next logo"
-            className="inline p-2 dark:rounded-md dark:bg-white"
-            height={0}
-            src="next.svg"
-            width={110}
-          />
-        )}
-        ! 😎
-      </h1>
-      <input
-        className="input input-primary"
-        id="nameInput"
-        type="text"
-        value={gs.loggedUser || ""}
-        onChange={(e) => set("loggedUser", e.target.value)}
-      />
-      <button className="btn mt-4 btn-primary" onClick={handleThemeToggle}>
-        <SunMoon className="mr-2" size={24} />
-        Toggle Theme
-      </button>
+    <div className="flex min-h-screen flex-col items-center bg-blue-100">
+        <div className="bg-indigo-400 flex items-center flex-col p-7.5 m-5 rounded-xl">
+            <h1 className="flex font-bold text-2xl">Üdvözlünk a TypeScript Project munkánk kezdő oldalán!!!</h1>
+        </div>
+        <div>
+                   <ul className="mt-3">
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link
+                className="rounded-full px-3 text-blue-500 hover:bg-gray-100 hover:text-red-500"
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        </div>
     </div>
-  );
+  )
 }
---
+++ app/szamolas/page.tsx
@@ -0,0 +1,28 @@
+type SearchParams = {
+  V1?: string;
+  V2?: string;
+  s?: string;
+  t1?: string;
+  t2?: string;
+};
+
+export default function KetAuto({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const V1 = searchParams.V1 ? Number(searchParams.V1) : 10;
+  const V2 = searchParams.V2 ? Number(searchParams.V2) : 15;
+  const s = searchParams.s ? Number(searchParams.s) : 20;
+
+  const t1 = searchParams.t1 ? Number(searchParams.t1) : s / V1;
+  const t2 = searchParams.t2 ? Number(searchParams.t2) : s / V2;
+
+
+
+  return (
+ <div className="flex justify-center min-h-screen bg-blue-100">
+  <h1 className=""> Üdvözlünk a Project munkánk</h1>
+ </div>
+  )
+}
